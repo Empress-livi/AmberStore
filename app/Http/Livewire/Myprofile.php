ba254a43dc85fea0969588a917d1d49c6e8de28c
Olivia Hales <halesolivia7@gamil.com>
2021-12-09 11:13:15 -0500

    profile page added

--- app/Http/Livewire/Myprofile.php
+++ app/Http/Livewire/Myprofile.php
@@ -6,6 +6,17 @@ use Livewire\Component;
 
 class Myprofile extends Component
 {
+    public $name, $email, $phone;
+
+    public function edit()
+    {
+        User::update([
+            'name'=> $this->name,
+            'address'=> $this->address,
+            'email'=> $this->email,
+            'phone'=> $this->phone
+        ]);
+    }
     public function render()
     {
         return view('livewire.myprofile');
--- resources/views/livewire/myprofile.blade.php
+++ resources/views/livewire/myprofile.blade.php
@@ -1,4 +1,35 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
-    
+    <div class="flex items-center h-screen w-full justify-center">
+
+        <div class="max-w">
+            <div class="bg-white shadow-xl rounded-lg py-3">
+                <div class="photo-wrapper p-2">
+                    <img class="w-32 h-32 rounded-full mx-auto" src="https://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp" alt="John Doe">
+                </div>
+                <div class="p-2">
+                    <h3 class="text-center text-xl text-gray-900 font-medium leading-8">Joh Doe</h3>
+                    <div class="text-center text-gray-400 text-xs font-semibold">
+                    </div>
+                    <table class="text-m my-3">
+                        <tbody>
+                            <td class="px-2 py-2 text-gray-500 font-semibold">Address</td>
+                            <td class="px-2 py-2 text-gray-500 font-semibold">Phone</td>
+                            <td class="px-2 py-2 text-gray-500 font-semibold">Email</td>
+                    </tbody>
+                    <tbody>
+                        <td class="px-2 py-2">Olivia</td>
+                        <td class="px-2 py-2">john@exmaple.com</td>
+                        <td class="px-2 py-2">+977 9955221114</td></tbody>
+                    </table>
+
+                    <div class="text-center my-2 space-x-4 ">
+                        <a class="text-m text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">Cart</a>
+                        <a class="text-m text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">Orders</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
 </div>
